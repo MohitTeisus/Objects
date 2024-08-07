bb60a0f14ece78660e65f20a72ba7fbcee9a8d41
Your Name <you@example.com>
2024-08-06 17:00:27 -0700

    New Changes

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float damping;
+
+    public Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        try
+        {
+            target = GameObject.FindWithTag("Player").transform;
+        }
+        catch (NullReferenceException e)
+        {
+
+        }
+
+
+        Vector3 targetPosition = target.position + offset;
+        targetPosition.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, damping);
+    }
+}
